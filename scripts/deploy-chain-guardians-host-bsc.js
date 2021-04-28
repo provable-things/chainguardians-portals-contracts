4e81993ee5e918d5c3c65da1244d00fc06e0ea1c
Alessandro Manfredi <alle.manfredi@gmail.com>
2021-04-28 10:23:28 +0200

    feat(scripts): adds deploy scripts

--- scripts/deploy-chain-guardians-host-bsc.js
+++ scripts/deploy-chain-guardians-host-bsc.js
@@ -2,12 +2,23 @@ const { ethers, upgrades } = require('hardhat')
 
 const main = async () => {
   const ChainGuardiansPortalsHost = await ethers.getContractFactory('ChainGuardiansPortalsHost')
+  const ChainGuardiansStorage = await ethers.getContractFactory('ChainGuardiansStorage')
+  console.info('Deploying ChainGuardiansStorage...')
+  const chainGuardiansStorage = await ChainGuardiansStorage.deploy()
+  console.info('ChainGuardiansStorage deployed to:', chainGuardiansStorage.address)
+
   console.info('Deploying ChainGuardiansPortalsHost...')
   const pnt = '0xdaacb0ab6fb34d24e8a67bfa14bf4d95d4c7af92'
-  const { address } = await upgrades.deployProxy(ChainGuardiansPortalsHost, ['pnt', 'name', 'symbol', 'uri'], {
-    initializer: 'initialize',
-  })
-  console.info('ChainGuardiansPortalsHost deployed to:', address)
+  const baseUri = 'https://api.chainguardians.io/api/opensea/'
+  const chainGuardiansPortalsHost = await upgrades.deployProxy(
+    ChainGuardiansPortalsHost,
+    [pnt, baseUri, chainGuardiansStorage.address],
+    {
+      initializer: 'initialize',
+    }
+  )
+
+  console.info('ChainGuardiansPortalsHost deployed to:', chainGuardiansPortalsHost.address)
 }
 
 main()
--- scripts/deploy-chain-guardians-native-eth.js
+++ scripts/deploy-chain-guardians-native-eth.js
@@ -2,12 +2,16 @@ const { ethers, upgrades } = require('hardhat')
 
 const main = async () => {
   const ChainGuardiansPortalsNative = await ethers.getContractFactory('ChainGuardiansPortalsNative')
+
   console.info('Deploying ChainGuardiansPortalsNative...')
-  const { address } = await upgrades.deployProxy(
-    ChainGuardiansPortalsNative,
-    ['cgt token', 'erc777 supported by pNetwork', 'vault'],
-    { initializer: 'initialize' }
-  )
+  const cgt = '0x3CD41EC039c1F2DD1f76144bb3722E7b503f50ab'
+  const pnt = '0x89Ab32156e46F46D02ade3FEcbe5Fc4243B9AAeD'
+  const vault = '0xAB83bD5169F58e753d291223dCaBa4F7644aD3a9'
+
+  const { address } = await upgrades.deployProxy(ChainGuardiansPortalsNative, [cgt, pnt, vault], {
+    initializer: 'initialize',
+  })
+
   console.info('ChainGuardiansPortalsNative deployed to:', address)
 }
 
